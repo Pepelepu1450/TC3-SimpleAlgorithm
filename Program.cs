55b788a410376fdcfe308ce5ba568b93697706c7
pepe <jose.vacaslocas@gmail.com>
2020-09-28 19:42:37 -0600

    practica para uso de algoritmos

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tc3
+{
+    class Program
+    {
+        static void Main (string[] args){
+            System.Console.WriteLine("numero");
+            int numero =Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[numero];
+            
+            Secuencia(arr, numero);
+            PrintArray(arr);
+        }
+
+        static void PrintArray(int[] nums){
+            for (int i = 0; i < nums.Length; i++)
+            {
+                System.Console.WriteLine(nums[i]);
+            }
+        }
+
+        static void Secuencia (int[] nums, int numero ){
+            for(int i = 0; i < numero; i++){
+                nums[i] = i+1;
+            }
+        }
+    }
+}
